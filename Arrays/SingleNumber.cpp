f19656bd9f5c638ac29eb39cf68f9b5725a6eaf0
namantalesra-fba <namanfba@gmail.com>
2026-04-01 22:41:30 +0530

    Add files via upload

---
+++ Arrays/SingleNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int arr[5]={2,3,4,2,4};
+    int i,j,sum=0;
+    for(i=0;i<5;i++){
+        sum^=arr[i];
+
+    }
+    cout<<sum<<endl;
+
+}
