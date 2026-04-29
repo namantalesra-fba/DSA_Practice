aa054e8f467c02e1a044918e4ef916d8a76e59a7
namantalesra-fba <namanfba@gmail.com>
2026-04-29 10:20:30 +0530

    Add files via upload

---
+++ Arrays/moore_algo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> vec={1,2,1,2,1};
+    int freq=0,ans=0;
+    for(int i=0;i<vec.size();i++){
+        if(freq==0){
+            ans=vec[i];
+        }
+        if(ans==vec[i]){
+            freq++;
+            
+        }else{
+            freq--;
+        }
+     
+    }cout<<ans<<endl;
+}
